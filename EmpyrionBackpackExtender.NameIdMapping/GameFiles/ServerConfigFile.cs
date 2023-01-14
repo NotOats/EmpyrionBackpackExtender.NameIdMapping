ecece864ac91fa443805ff934dcfd39ceb6cbd3a
NotOats <ian@aotc.us>
2023-01-14 16:01:22 -0500

    Add project files.

---
+++ EmpyrionBackpackExtender.NameIdMapping/GameFiles/ServerConfigFile.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace EmpyrionBackpackExtender.NameIdMapping.GameFiles
+{
+    /// <summary>
+    /// Represents an immutable server config file, typically named Dedicated.yaml
+    /// </summary>
+    public class ServerConfigFile
+    {
+        // ServerConfig
+        /// <summary>
+        /// The server's name
+        /// </summary>
+        public string ServerName { get; }
+
+        /// <summary>
+        /// The save game directory, typically "<SERVER_ROOT>\Saves\"
+        /// </summary>
+        public string SaveDirectory { get; }
+
+        /// <summary>
+        /// The AdminConfig file, typically "<SaveDirectory>\adminconfig.yaml"
+        /// </summary>
+        public string AdminConfigFile { get; }
+
+
+        // GameConfig
+        /// <summary>
+        /// The name of the game's save directory, located at "<SaveDirectory>\Games\<GameName>\"
+        /// </summary>
+        public string GameName { get; }
+
+        /// <summary>
+        /// Name of the scenario the server runs.
+        /// Used to locate the scenario folder at "<SERVER_ROOT>\Content\Scenarios\<CustomScenario>\"
+        /// </summary>
+        public string CustomScenario { get; }
+
+        private ServerConfigFile(
+            string serverName,
+            string saveDirectory,
+            string adminConfigFile,
+            string gameName,
+            string customScenario)
+        {
+            ServerName = serverName;
+            SaveDirectory = saveDirectory ?? "Saves";
+            AdminConfigFile = adminConfigFile ?? "adminconfig.yaml";
+
+            GameName = gameName;
+            CustomScenario = customScenario;
+        }
+
+        /// <summary>
+        /// Reads a server's dedicated config file, typically located at "<SERVER_ROOT>\dedicated.yaml"
+        /// </summary>
+        /// <param name="file">The config file to load</param>
+        /// <returns></returns>
+        public static ServerConfigFile Load(string file)
+        {
+            if (!File.Exists(file))
+                return null;
+
+            var deserializer = new DeserializerBuilder()
+                .IgnoreUnmatchedProperties()
+                .Build();
+
+            using (var reader = new StreamReader(file))
+            {
+                var builder = deserializer.Deserialize<ServerConfigFileBuilder>(reader);
+
+                return builder.Build();
+            }
+        }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        private class ServerConfigFileBuilder
+        {
+            public ServerConfigSection ServerConfig { get; set; }
+            public GameConfigSection GameConfig { get; set; }
+
+            public ServerConfigFile Build()
+            {
+                return new ServerConfigFile(
+                    ServerConfig.ServerName,
+                    ServerConfig.SaveDirectory,
+                    ServerConfig.AdminConfigFile,
+                    GameConfig.GameName,
+                    GameConfig.CustomScenario);
+            }
+
+            public class ServerConfigSection
+            {
+                [YamlMember(Alias = "Srv_Name")]
+                public string ServerName { get; set; }
+                public string SaveDirectory { get; set; }
+                public string AdminConfigFile { get; set; }
+            }
+
+            public class GameConfigSection
+            {
+                public string GameName { get; set; }
+                public string CustomScenario { get; set; }
+            }
+        }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    }
+}
---
+++ EmpyrionBackpackExtender.NameIdMapping/Program.cs
@@ -0,0 +1,109 @@
+﻿using EmpyrionBackpackExtender.NameIdMapping.GameFiles;
+using Newtonsoft.Json;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+AnsiConsole.Write(new FigletText("NameIdMapping Generator").Centered());
+
+var app = new CommandApp<DumpNameIdMappingFile>();
+return app.Run(args);
+
+class GameSettings : CommandSettings
+{
+    [CommandArgument(0, "[server_folder]")]
+    [Description("The server's root folder (where 'BuildNumber.txt' is located)")]
+    public string? ServerFolder { get; set; }
+
+    [CommandArgument(1, "[server_config]")]
+    [Description("The server's configuration file (typically dedicated.yaml)")]
+    public string? ServerConfig { get; set; }
+
+
+    [CommandArgument(2, "[ecf_files]")]
+    [Description("A List of ecf files to read Item & Blocks from")]
+    public string[]? EcfFiles { get; set; }
+
+    public void PromptForMissing()
+    {
+        ServerFolder ??= AnsiConsole.Ask<string>("What is your server's [green]root folder[/] (where 'BuildNumber.txt' is located)?");
+
+        ServerConfig ??= AnsiConsole.Ask<string>("What is your server's [green]configuration file[/]?", "dedicated.yaml");
+
+        // Pull available list for selection
+        if (EcfFiles == null || EcfFiles.Length == 0)
+        {
+            var save = new SaveGame(ServerFolder, ServerConfig);
+            string[] options = save.ScenarioEcfFiles().Select(Path.GetFileName).Where(file => file != null).ToArray()!;
+            var defaults = new[] { "Config_RE.ecf", "BlocksConfig.ecf", "ItemsConfig.ecf" }.Where(options.Contains);
+
+            var files = AnsiConsole.Prompt(
+                new MultiSelectionPrompt<string>()
+                    .PageSize(10)
+                    .Title("Which ECF Files should should I use?")
+                    .MoreChoicesText("[gray](Move up and down to reveal more files)[/]")
+                    .InstructionsText("[grey](Press [blue]space[/] to toggle a file, [green]enter[/] to accept)[/]")
+                    .AddChoiceGroup("Defaults", defaults)
+                    .AddChoices(options.Where(file => !defaults.Contains(file)))
+                );
+
+            EcfFiles = files.ToArray();
+        }
+    }
+}
+
+class DumpNameIdMappingFile : Command<GameSettings>
+{
+    public override int Execute([NotNull] CommandContext context, [NotNull] GameSettings settings)
+    {
+        // Prompt for missing settings
+        settings.PromptForMissing();
+
+        // Load game files
+        var save = new SaveGame(settings.ServerFolder!, settings.ServerConfig!);
+
+        // Dump save info
+        var uiPaths = new Rows(
+            new Text($"Root Folder: {save.ServerDirectory}"),
+            new Text($"Game Name:   {save.ServerConfig.GameName}"),
+            new Text($"Scenario:    {save.ServerConfig.CustomScenario}"));
+
+        AnsiConsole.Write(new Panel(uiPaths).Header("Server Information").Expand());
+
+        // Timer
+        var sw = Stopwatch.StartNew();
+
+        // Do Map things
+        var map = save.CreateRealIdToNameMap(settings.EcfFiles!);
+        AnsiConsole.WriteLine($"Generated Real Id <-> Name map with {map.Count} entries");
+
+        // Change to the correct dictionary found in GitHub-TC/EmpyrionScripting in ConfigEcfAccess.cs 
+        // https://github.com/GitHub-TC/EmpyrionScripting/blob/a4f6073e812f38ab5ad90534bf7e2402ac15920d/EmpyrionScripting/ConfigEcfAccess.cs#L16
+        //
+        // public IReadOnlyDictionary<string, int> BlockIdMapping { get; set; }
+
+        var invertedMap = map.ToDictionary(x => x.Value, x => x.Key).OrderBy(kvp => kvp.Value);
+
+        // Write to file
+        using StreamWriter writer = File.CreateText("NameIdMapping.json");
+
+        var serializer = JsonSerializer.Create(new JsonSerializerSettings() { Formatting = Formatting.Indented });
+        serializer.Serialize(writer, invertedMap);
+
+        // Report
+        sw.Stop();
+
+        AnsiConsole.MarkupLine($"[green]Finished![/] Generated NameIdMapping.json in {sw.ElapsedMilliseconds:0.00}ms");
+
+        // Write helpfull directions
+        var modFolder = Path.Join(save.SaveGameDirectory, @"Mods\EmpyrionBackpackExtender");
+        AnsiConsole.WriteLine("To enable mapping in EmyprionBackpackExtender please do the following.");
+        AnsiConsole.WriteLine($"1.) Place NameIdMapping.json in {modFolder}");
+        AnsiConsole.WriteLine("2.) Open Configuration.json (in the same folder) with a text editor of your choice.");
+        AnsiConsole.MarkupLine("3.) Set \"NameIdMappingFile\" equal to the [red]full[/] path of NameIdMapping.json");
+
+        return 0;
+    }
+}
